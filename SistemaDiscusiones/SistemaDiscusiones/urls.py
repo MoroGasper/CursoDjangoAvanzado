4752f9bce2118f5998a8ce6708549ff66f882bda
Gustavo Gil <ggprado@gmail.com>
2014-08-02 14:49:29 -0430

    Social Auth!!!

--- SistemaDiscusiones/SistemaDiscusiones/urls.py
+++ SistemaDiscusiones/SistemaDiscusiones/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'SistemaDiscusiones.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'', include('apps.home.urls')),
+#    url(r'', include('apps.users.urls', namespace='users')),
+    # Python Social Auth
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 )
